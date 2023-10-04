9406b03ce70950b4a9746ed32dde0c11e9dbe116
Pedro Teixeira <1211184@isep.ipp.pt>
2023-10-04 11:16:20 +0100

    bulletproof project demo

---
+++ bulletproof_demo/src/core/domain/UniqueEntityID.ts
@@ -0,0 +1,10 @@
+
+import uuid from 'uuid/v4';
+const { v4: uuidV4 } = require('uuid');
+import { Identifier } from './Identifier'
+
+export class UniqueEntityID extends Identifier<string | number>{
+  constructor (id?: string | number) {
+    super(id ? id : uuid())
+  }
+}
