7398955b25c46a105598ea8ebc89a4b1c63af8bf
Pedro Teixeira <1211184@isep.ipp.pt>
2023-11-14 09:54:30 +0000

    #64 initial progress

---
+++ SPA/src/app/component/robot/deactivate-robot/deactivate-robot.component.ts
@@ -0,0 +1,18 @@
+import {Component, inject, OnInit} from '@angular/core';
+import {RobotService} from "../../../services/robot/robot.service";
+
+@Component({
+    selector: 'app-deactivate-robot',
+    templateUrl: './deactivate-robot.component.html',
+    styleUrls: ['./deactivate-robot.component.css']
+})
+export class DeactivateRobotComponent implements OnInit {
+
+    service = inject(RobotService);
+    ngOnInit(): void {
+    }
+
+    constructor() {
+    }
+
+}
