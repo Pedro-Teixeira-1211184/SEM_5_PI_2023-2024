4ccd8b4d5c3ab798b62aeda7887fbdfe61257814
Pedro Teixeira <1211184@isep.ipp.pt>
2023-11-16 19:26:35 +0000

    #36 form still incomplete

--- SPA/src/app/component/map/patch-map/patch-map.component.ts
+++ SPA/src/app/component/map/patch-map/patch-map.component.ts
@@ -4,6 +4,8 @@ import {BuildingService} from "../../../services/building/building.service";
 import {MapService} from "../../../services/map/map.service";
 import {FormControl, FormGroup, Validators} from "@angular/forms";
 import {PassagewayService} from "../../../services/passageway/passageway.service";
+import IFloorDTO from "../../floor/dto/IFloorDTO";
+import IBuildingDTO from "../../building/list-building/dto/IBuildingDTO";
 
 @Component({
   selector: 'app-patch-map',
@@ -15,10 +17,12 @@ export class PatchMapComponent implements OnInit {
   service = inject(MapService);
   b_service = inject(BuildingService);
   p_service = inject(PassagewayService);
+  f_service = inject(FloorService);
 
   mapForm!: FormGroup;
 
-  buildingsCodes: string[] = [];
+  buildings: IBuildingDTO[] = [];
+  floors: IFloorDTO[] = [];
 
   matrix: number[][] = [];
   columns: number = 3;
@@ -31,51 +35,48 @@ export class PatchMapComponent implements OnInit {
   ngOnInit(): void {
     this.mapForm = new FormGroup({
       buildingCode: new FormControl('', [Validators.required]),
-      floorNumber: new FormControl('', [Validators.required]),
-      length: new FormControl('', [Validators.required]),
-      width: new FormControl('', [Validators.required]),
+      floorCode: new FormControl('', [Validators.required]),
     });
     this.initializeMatrix();
   }
 
   initializeMatrix() {
-    this.matrix = Array.from({ length: this.columns }, () =>
-      Array.from({ length: this.rows }, () => 0)
+    this.matrix = Array.from({length: this.columns}, () =>
+      Array.from({length: this.rows}, () => 0)
     );
   }
 
   private async getBuildingsCodes() {
-    this.buildingsCodes = await this.b_service.getAllBuildingsCode();
+    this.buildings = await this.b_service.getAllBuildings();
+  }
+
+  private async getFloorsCodes(buildingCode: string) {
+    this.floors = [];
+    this.floors = await this.f_service.getFloorsByBuildingCodeForPassageway(buildingCode);
   }
 
   get buildingCode() {
     return this.mapForm.get('buildingCode');
   }
 
-  get floorNumber() {
-    return this.mapForm.get('floorNumber');
-  }
-
-  get length() {
-    return this.mapForm.get('length');
-  }
-
-  get width() {
-    return this.mapForm.get('width');
+  get floorCode() {
+    return this.mapForm.get('floorCode');
   }
 
   public async submit() {
-    console.log(this.columns);
-    console.log(this.rows);
+    //TODO: submit
   }
 
-  confirmLength() {
-    this.columns = this.length?.value;
+  public async getFloorsOfBuilding() {
+    //set grid dimensions
+    for (let i = 0; i < this.buildings.length; i++) {
+      if (this.buildings[i].code == this.buildingCode?.value) {
+        this.columns = this.buildings[i].dimensions.length + 1;
+        this.rows = this.buildings[i].dimensions.width + 1;
+        break;
+      }
+    }
     this.initializeMatrix();
-  }
-
-  confirmWidth() {
-    this.rows = this.width?.value;
-    this.initializeMatrix();
+    await this.getFloorsCodes(this.buildingCode?.value);
   }
 }
