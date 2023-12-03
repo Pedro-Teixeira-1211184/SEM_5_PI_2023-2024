8ba3219dc519e425744f25659664299916cf8352
Pedro Gonçalo Pereira e Teixeira <1211184@isep.ipp.pt>
2023-12-03 00:40:39 +0000

    #86 #87 #88 #89 #90 rgpd content updated

--- MV/src/app/components/user/register/register.component.ts
+++ MV/src/app/components/user/register/register.component.ts
@@ -1,56 +1,59 @@
 import {Component, inject} from '@angular/core';
 import {FormControl, FormGroup, Validators} from "@angular/forms";
-import {AuthService} from "../../../services/auth/auth.service";
-import Constants from "../../../../utils/Constants";
+import {AuthService} from "../../services/auth.service";
+import Constants from "../../../utils/Constants";
 
 @Component({
-  selector: 'app-register',
-  templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+    selector: 'app-register',
+    templateUrl: './register.component.html',
+    styleUrls: ['./register.component.css']
 })
 export class RegisterComponent {
-  ngOnInit(): void {
-    this.form = new FormGroup({
-      firstName: new FormControl('', Validators.required),
-      lastName: new FormControl('', Validators.required),
-      email: new FormControl('', [Validators.required, Validators.email]),
-      password: new FormControl('', Validators.required)
-    });
-  }
+    ngOnInit(): void {
+        this.form = new FormGroup({
+            firstName: new FormControl('', Validators.required),
+            lastName: new FormControl('', Validators.required),
+            email: new FormControl('', [Validators.required, Validators.email]),
+            password: new FormControl('', Validators.required)
+        });
+    }
 
-  service: AuthService = inject(AuthService);
+    service: AuthService = inject(AuthService);
 
-  form!: FormGroup;
-  conditions = true;
+    form!: FormGroup;
+    conditions = true;
 
-  constructor() {
-  }
+    constructor() {
+    }
 
-  get firstName() {
-    return this.form.get('firstName');
-  }
+    get firstName() {
+        return this.form.get('firstName');
+    }
 
-  get lastName() {
-    return this.form.get('lastName');
-  }
+    get lastName() {
+        return this.form.get('lastName');
+    }
 
-  get email() {
-    return this.form.get('email');
-  }
+    get email() {
+        return this.form.get('email');
+    }
 
-  get password() {
-    return this.form.get('password');
-  }
+    get password() {
+        return this.form.get('password');
+    }
 
-  public async submit() {
-    await this.service.signUp(this.firstName?.value, this.lastName?.value, this.email?.value, this.password?.value, Constants.USER_ROLE);
-  }
+    public async submit() {
+        // just if first name and last name are not empty just as email and password
+        if (!this.form.invalid) {
+            await this.service.signupRequest(this.firstName?.value, this.lastName?.value, this.email?.value, this.password?.value);
+        }
+    }
 
-  accept() {
-    this.conditions = false;
-  }
+    accept() {
+        this.conditions = false;
+    }
 
-  decline() {
-    window.location.href = "/home";
-  }
+    decline() {
+        window.location.href = "/home";
+    }
 }
